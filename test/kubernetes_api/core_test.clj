333a23d1e8e2af664e88cebe345c296f778bb437
drew verlee <drew.verlee@gmail.com>
2020-06-09 09:48:10 -0500

    debug - failing to create client

--- test/kubernetes_api/core_test.clj
+++ test/kubernetes_api/core_test.clj
@@ -7,7 +7,8 @@
 
 (comment
 
-  (def k8s (k8s-api/client "https://localhost" {}))
+  ;;I expect this to create a client if i have kubectl proxy running but it doesn't
+  (def k8s (k8s-api/client "http://localhost:8001/api/" {}))
 
 
   ;;(k8s-api/explore k8s :Deployment)
